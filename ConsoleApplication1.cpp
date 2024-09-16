621a0f6129f5d7841674cb2c2a05e9658204e613
Nikita4501 <butakovnikita000@mail.ru>
2024-09-16 04:13:53 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int int1 = 35;
+    int int2 = 20;
+    int int3 = 15;
+    int int4 = 25;
+    int int5 = int1 + int2;
+    int int6 = int1 - int2;
+    int int7 = int1 * int2;
+    int int8 = int1 / int2;
+    int int9 = int1 % int2;
+    cout << int3 << endl << int4 << endl << int5 << endl << int6 << endl << int7 << endl;
+    if (int1 == int2)
+    {
+        cout << "operator == " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator == " << "false" << endl;
+    }
+    if (int1 != int2)
+    {
+        cout << "operator != " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator != " << "false" << endl;
+    }
+    if (int1 > int2)
+    {
+        cout << "operator > " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator > " << "false" << endl;
+    }
+    if (int1 < int2)
+    {
+        cout << "operator < " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator < " << "false" << endl;
+    }
+    if (int1 >= int2)
+    {
+        cout << "operator >= " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator >= " << "false" << endl;
+    }
+    if (int1 <= int2)
+    {
+        cout << "operator <= " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator <= " << "false" << endl;
+    }
+    if (!(int1 == int2))
+    {
+        cout << "operator ! == " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator ! == " << "false" << endl;
+    }
+    if (int1 != int3 && int2 == int4)
+    {
+        cout << "operator != && == " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator != && == " << "false" << endl;
+    }
+    if (int1 == int3 || int2 != int4)
+    {
+        cout << "operator == || != " << "true" << endl;
+    }
+    else
+    {
+        cout << "operator == || != " << "false" << endl;
+    }
+}
